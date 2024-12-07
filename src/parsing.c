6a3e4ccb6f45ba0cc1c001674b2f2a34562352c2
ndavenne <github@noedavenne.aleeas.com>
2024-12-07 12:10:24 +0100

    added main args checking

--- src/parsing.c
+++ src/parsing.c
@@ -6,15 +6,39 @@
 /*   By: ndavenne <ndavenne@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 17:26:16 by ndavenne          #+#    #+#             */
-/*   Updated: 2024/12/06 18:01:29 by ndavenne         ###   ########.fr       */
+/*   Updated: 2024/12/07 11:43:08 by ndavenne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	parse_args(argc, argv)
+bool	check_digit_string(const char *str)
 {
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (ft_isdigit(str[i]) == false && str[i] != ' ')
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
+void	check_args(int argc, char *argv[])
+{
+	int	i;
+
 	if (argc < 2)
 		ft_error(ERR_USAGE);
-
+	if (argc == 2 && argv[1][0] == '\0')
+		ft_error(ERR_EMPTY);
+	i = 1;
+	while (i < argc)
+	{
+		if (check_digit_string(argv[i]) == false)
+			ft_error(ERR_INVALID_STRING);
+		i++;
+	}
 }
