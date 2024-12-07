08ffc3716934d206c573dcd667a97ad0ba65e88e
ndavenne <github@noedavenne.aleeas.com>
2024-12-07 12:09:57 +0100

    added various error messahes

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,27 +6,27 @@
 /*   By: ndavenne <ndavenne@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 15:46:46 by ndavenne          #+#    #+#             */
-/*   Updated: 2024/12/06 17:57:16 by ndavenne         ###   ########.fr       */
+/*   Updated: 2024/12/07 11:43:05 by ndavenne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+# include <stdbool.h>
 # include "libndav.h"
+
+//test
+# include <stdio.h>
 
 typedef enum e_error
 {
 	ERR_OK,
-	ERR_USAGE
+	ERR_USAGE,
+	ERR_EMPTY,
+	ERR_INVALID_STRING,
+	ERR_MAX
 }	t_error;
-
-typedef struct s_ps
-{
-	int			number;
-	struct s_ps	*prev;
-	struct s_ps	*next;
-}	t_ps;
 
 void	ft_error(t_error error_code);
 
