a1489a6610133dae518432ad9b3ecc0aed760864
ndavenne <github@noedavenne.aleeas.com>
2024-12-15 02:48:15 +0100

    re normed

--- src/algo/rotation_instructions.c
+++ src/algo/rotation_instructions.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   operation_selection.c                              :+:      :+:    :+:   */
+/*   rotation_instructions.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ndavenne <ndavenne@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 15:21:59 by ndavenne          #+#    #+#             */
-/*   Updated: 2024/12/15 02:37:57 by ndavenne         ###   ########.fr       */
+/*   Updated: 2024/12/15 02:47:52 by ndavenne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,21 +66,21 @@ t_rotation_instruction	select_best_rotation(t_rotation_costs rc)
 	return (instructions);
 }
 
-t_rotation_instruction	find_best_target(int reference, t_stack *s, t_stack *d,
-	t_compare_function compare)
+t_rotation_instruction	rotation_instructions(
+	int reference, t_stack *src, t_stack *dest, t_compare_function compare)
 {
 	t_rotation_costs	rc;
 	int					source_index;
 	int					target_index;
 
-	source_index = get_index(reference, s->list);
-	target_index = get_index(find_target(reference, d, compare), d->list);
+	source_index = get_index(reference, src->list);
+	target_index = get_index(find_target(reference, dest, compare), dest->list);
 	if (source_index == -1 || target_index == -1)
 		ft_error(ERR_NO_NUMBER);
 	rc.source_rotate = source_index;
-	rc.source_rrotate = s->len - source_index;
+	rc.source_rrotate = src->len - source_index;
 	rc.target_rotate = target_index;
-	rc.target_rrotate = d->len - target_index;
+	rc.target_rrotate = dest->len - target_index;
 	rc.double_rotate = max(rc.source_rotate, rc.target_rotate);
 	rc.double_rrotate = max(rc.source_rrotate, rc.target_rrotate);
 	rc.opposite_rotate = rc.source_rotate + rc.target_rrotate;
@@ -88,8 +88,6 @@ t_rotation_instruction	find_best_target(int reference, t_stack *s, t_stack *d,
 	return (select_best_rotation(rc));
 }
 
-//	ft_dprintf(STDOUT_FILENO, "target for %3d is %3d and cost %d\n",
-// list->number, (list->number, dest, compare), current.total_cost);
 t_rotation_instruction	find_best_instructions(t_stack *src, t_stack *dest,
 	t_compare_function compare)
 {
@@ -101,10 +99,13 @@ t_rotation_instruction	find_best_instructions(t_stack *src, t_stack *dest,
 	cheapest = (t_rotation_instruction){.total_cost = INT32_MAX};
 	while (list != NULL)
 	{
-		current = find_best_target(list->number, src, dest, compare);
+		current = rotation_instructions(list->number, src, dest, compare);
 		if (current.total_cost < cheapest.total_cost)
 			cheapest = current;
 		list = list->next;
 	}
 	return (cheapest);
 }
+
+//	ft_dprintf(STDOUT_FILENO, "target for %3d is %3d and cost %d\n",
+// list->number, (list->number, dest, compare), current.total_cost);
